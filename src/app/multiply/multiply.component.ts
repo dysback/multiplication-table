8fded4c83968b69c63866396544882509697fb44
Damir Zelenika <damir.zelenika@gmail.com>
2024-10-23 15:52:25 +0200

    Working examplke before bootstrap

--- src/app/multiply/multiply.component.ts
+++ src/app/multiply/multiply.component.ts
@@ -1,8 +1,8 @@
 import { ValueChangeEvent } from '@angular/forms';
 
 import { FormsModule } from '@angular/forms'; // Import the FormsModule
-import { Component } from "@angular/core";
-import { callAfter, randomInt } from './utils/basic-utils';
+import { Component, ElementRef, QueryList, TemplateRef, ViewChild, ViewChildren, afterRender } from "@angular/core";
+import { PromiseCallAfter, randomInt, Callback } from './utils/basic-utils';
 
 @Component({
   selector: 'app-multiply',
@@ -11,48 +11,90 @@ import { callAfter, randomInt } from './utils/basic-utils';
   templateUrl: './multiply.component.html',
   styleUrl: './multiply.component.css'
 })
-export class MultiplyComponent {
-  numbers: Array<Pair>=[];
-  //answers: Array<number>=[];
 
+export class MultiplyComponent {
+  @ViewChild("myAnswer") txtAnswer!:ElementRef;
+
+  constructor() {
+    afterRender(() => {
+      //console.log("AR " + this.txtAnswer);
+      this.txtAnswer.nativeElement.focus();
+    });
+  }
+
+
+  numbers: Array<Pair>=[];
   answer:number|null = null;
   size = 10;
   speed = 5;
   questions = 10;
   reminder = 0;
   question = 0;
-
-
+  fn:any;
+  animate:any;
   x: number|null = null;
   y: number|null = null;
-  start = () => {
+  //txtAnswer: any;
+
+
+  start = async () => {
     this.numbers = [];
     for(var i = 0; i < this.questions; i++) {
-      this.numbers.push({x: randomInt(this.size), y: randomInt(this.size)})
+      this.numbers.push({x: randomInt(this.size), y: randomInt(this.size), answered: false, correct: false})
     }
     console.log(this.numbers);
-    this.displayNumber(this.question);
+
+    const pDisplayNumber = (index: number) => {return new Promise(async (resolve, reject) => {
+      await this.displayNumber(index);
+      resolve('Gut');
+    })};
+    for(this.question = 0; this.question < this.numbers.length; this.question++) {
+      var r = await pDisplayNumber(this.question);
+      this.answer = null;
+      console.log("R" + r + "/"+ this.question);
+    }
   }
 
-  displayNumber = (index: number) => {
+  displayNumber = async (index: number) => {
     this.reminder = this.speed;
     this.x = this.numbers[index].x;
     this.y = this.numbers[index].y;
-    
-    callAfter(this.speed * 1000, this.fn, ['Gotovoooooo'], 1000, this.decreaseReminder);
+    //console.log(this.txtAnswer.nativeElement);
+    this.txtAnswer.nativeElement.focus();
+
+    this.fn = new Callback(this.finalFn, this.speed * 1000, ['You Mised!!!']);
+    this.animate = new Callback(this.decreaseReminder, 200);
+    await PromiseCallAfter(this.fn, this.animate);
+    this.numbers[index].answered = true;
+    //this.numbers[index].correct = false;
+
   }
 
-  fn = (x:string) => {
+  finalFn = (x:string) => {
     alert(x);
 
   }
+
   decreaseReminder = () => {
     this.reminder--;
+    console.log("TR:" + this.reminder)
+  }
+
+  guess = () => {
+    if(<number>this.answer == this.numbers[this.question].x *  this.numbers[this.question].y) {
+      this.numbers[this.question].answered = true;
+      this.numbers[this.question].correct = true;
+      alert("Woooobraaaa");
+      this.fn.stop = 1;
+    }
   }
 }
 
 class Pair {
-  x!:number;
-  y!:number;
+  x!: number;
+  y!: number;
+  answered: boolean=false;
+  correct!: boolean;
 }
+
 
--- src/app/multiply/utils/basic-utils.ts
+++ src/app/multiply/utils/basic-utils.ts
@@ -1,37 +1,80 @@
-export function randomInt(max: number) : number {
+export function randomInt(max: number): number {
     return Math.floor(Math.random() * max);
 }
 
+export class Callback{
+    interval: number = 200;
+    function!: any;
+    params: Array<any> = [];
+    stop: number = 0;
 
-export function callAfter(mSec: number,  callFn: any, args: Array<any>=[],
-                            intervalMs: number=1000, intervalFn: any = null, intervalArgs : Array<any>=[]) {
-    if(mSec > 0) {
+    constructor(fn:any, interval:number=200, params:Array<any>=[]) {
+      this.function = fn;
+      this.interval = interval;
+      this.params = params;
+    }
+  }
+
+
+export async function PromiseCallAfter(fn:Callback, animate:Callback) {
+
+    const iPromise = new Promise((resolve, reject) => {
+        if (fn.interval > 0 && !fn.stop) {
+            animate.function(...animate.params);
+            setTimeout(() => resolve('OK'), animate.interval);
+        } else if (fn.stop) {
+            resolve('end');
+        } else {
+            fn.function(...fn.params);
+            resolve('end');
+        }
+    });
+
+    await iPromise.then(async status => {
+        if (status == 'OK') {
+            fn.interval = fn.interval - animate.interval;
+            await PromiseCallAfter(fn, animate);
+        }
+
+    })
+}
+
+
+
+
+
+//-------------------------------------
+export async function __PromiseCallAfter(mSec: number, callFn: any, args: Array<any> = [],
+    intervalMs: number = 1000, intervalFn: any = null, intervalArgs: Array<any> = []) {
+
+    console.log(mSec);
+
+    const iPromise = new Promise((resolve, reject) => {
+        if (mSec > 0) {
+            intervalFn(...intervalArgs);
+            setTimeout(() => resolve('OK'), intervalMs);
+        } else {
+            callFn(...args);
+            resolve('end');
+        }
+    });
+
+    await iPromise.then(async status => {
+        if (status == 'OK') {
+            await __PromiseCallAfter(mSec - intervalMs, callFn, args, intervalMs, intervalFn, intervalArgs);
+        }
+
+    })
+}
+
+export function __callAfter(mSec: number, callFn: any, args: Array<any> = [],
+    intervalMs: number = 1000, intervalFn: any = null, intervalArgs: Array<any> = []) {
+    if (mSec > 0) {
         console.log(mSec);
         setTimeout(intervalFn, 0, ...intervalArgs);
-        setTimeout(callAfter, intervalMs, mSec - intervalMs, callFn, args, intervalMs, intervalFn, intervalArgs);
+        setTimeout(__callAfter, intervalMs, mSec - intervalMs, callFn, args, intervalMs, intervalFn, intervalArgs);
         return;
     }
     setTimeout(callFn, 1000, ...args);
 
 }
-
-export function PromiseCallAfter(mSec: number,  callFn: any, args: Array<any>=[],
-    intervalMs: number=1000, intervalFn: any = null, intervalArgs : Array<any>=[]) {
-if(mSec > 0) {
-console.log(mSec);
-
-const waiti = (ms:number) => new  Promise(resolve => setTimeout(resolve, ms));
-waiti(0).then(() => {
-    const wait = (ms:number) => new  Promise(promiseCallAfter => setTimeout(promiseCallAfter, ms));
-    //setTimeout(callAfter, intervalMs, mSec - intervalMs, callFn, args, intervalMs, intervalFn, intervalArgs);
-})
-
-
-//setTimeout(intervalFn, 0, ...intervalArgs);
-return;
-}
-setTimeout(callFn, 1000, ...args);
-
-}
-
-
